0957cf4c122617af6301ab8e7f818c84ddd0d01c
Nia Smith <niasmith94@outlook.com>
2020-08-04 12:55:00 -0400

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+
 // This is our API key
-var APIKey = "";
+var APIKey = "40e532b573b9b839043237397439fa01";
 
 // Here we are building the URL we need to query the database
-var queryURL = "" + APIKey;
+var queryURL = "https://home.openweathermap.org/data/2.5/weather?q=" + APIKey;
+
+
+$("#search").click(function(e) {
+  var weather = $(#city).va;();
+  console.log(weather);
+})
+
 
 // Here we run our AJAX call to the OpenWeatherMap API
 $.ajax({
@@ -17,4 +25,5 @@ $.ajax({
     // Transfer Content to HTML using jQuery
     // console.log your data response
 
+    
   });
